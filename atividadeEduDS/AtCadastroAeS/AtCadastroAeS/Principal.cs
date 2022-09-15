ca926dca6bdc295f9637e4558abd4402e59d77c4
PedroLeGarcia <91338154+PedroLeGarcia@users.noreply.github.com>
2022-09-15 15:26:37 -0300

    Add files via upload
    eduu

---
+++ atividadeEduDS/AtCadastroAeS/AtCadastroAeS/Principal.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtCadastroAeS
+{
+    public partial class Principal : Form
+    {
+        public struct Usuar{
+            public int codigo;
+            public string nome;
+            public string nivel;
+            public string login;
+            public string senha;
+        }
+
+        static public Usuar[] usuarios = new Usuar[10];
+        static public int contUsuario = 0;
+
+        public struct CadasC{
+            public int codigo;
+            public string nome;
+            public string cpf;
+            public string rg;
+            public string end;
+            public string bairro;
+            public string cidade;
+            public string uf;
+            public string telefone;
+            public string email;
+            public string cep;
+        }
+
+        static public CadasC[] cadastro = new CadasC[10];
+        static public int contCadastro= 0;
+
+        public struct CadasP{
+            public int codigo;
+            public string desc;
+            public string uni;
+            public string qtd;
+            public string pcC;
+            public string pcV;
+        }
+
+        static public CadasP[] cadastrop = new CadasP[10];
+        static public int contCadastrop = 0;
+
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void usuario_Click(object sender, EventArgs e)
+        {
+            Usuario fu = new Usuario();
+            fu.ShowDialog();
+        }
+
+        private void cliente_Click(object sender, EventArgs e)
+        {
+            CadastroC lol = new CadastroC();
+            lol.ShowDialog();
+        }
+
+        private void produtos_Click(object sender, EventArgs e)
+        {
+            CadastroP lmao = new CadastroP();
+            lmao.ShowDialog();
+        }
+
+        
+
+        private void pdUsuario_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string strDados = "";
+            Graphics objImpressao = e.Graphics;
+            int pag = 0, linha = 0, i = 0;
+            bool cabecalho = true, itens = true;
+
+            while (cabecalho)
+            {
+                strDados = "                           RELATÓRIO DE USUÁRIOS" + (char)10;
+                strDados = "Data: " + DateTime.Now.ToString("dd/MM/yyyy") + "                                          Pág:" + pag.ToString("00") + (char)10;
+                strDados = "------------------------------------------------------------" + (char)10;
+                strDados = "Código nome                                  Nível Login" + (char)10;
+                strDados= "------------------------------------------------------------" + (char)10;
+                linha = 5;
+                itens = true;
+                while (itens)
+                {
+                    strDados += usuarios[i].codigo.ToString("000000") + " " + usuarios[i].nome.PadRight(40) + "   " + usuarios[i].nivel + "     " + usuarios[i].login + (char)10;
+                    linha++;
+                    i++;
+                    if (linha >= 64)
+                    {
+                        itens = false;
+                        strDados += (char)12;
+                    } 
+                    if(i>= contUsuario)
+                    {
+                        itens = false;
+                        cabecalho = false;
+                    }
+                }
+                objImpressao.DrawString(strDados, new Font("Courier New", 10, FontStyle.Regular), Brushes.Black, 50, 50);
+            } 
+                    
+        }
+        private void usuario2_Click(object sender, EventArgs e)
+        {
+            ppdUsuario.ShowDialog();
+        }
+    }
+}

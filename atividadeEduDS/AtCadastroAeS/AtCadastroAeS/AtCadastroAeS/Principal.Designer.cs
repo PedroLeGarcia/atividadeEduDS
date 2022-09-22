f074e8b98deacc15f3b04cd75a5c6f09cf003682
PedroLeGarcia <91338154+PedroLeGarcia@users.noreply.github.com>
2022-09-22 15:55:34 -0300

    Add files via upload

---
+++ atividadeEduDS/AtCadastroAeS/AtCadastroAeS/AtCadastroAeS/Principal.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace AtCadastroAeS
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuario = new System.Windows.Forms.ToolStripMenuItem();
+            this.cliente = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtos = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatoriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuario2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cliente2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtos2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sair = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pdUsuario = new System.Drawing.Printing.PrintDocument();
+            this.ppdUsuario = new System.Windows.Forms.PrintPreviewDialog();
+            this.pdCliente = new System.Drawing.Printing.PrintDocument();
+            this.ppdCliente = new System.Windows.Forms.PrintPreviewDialog();
+            this.pdProduto = new System.Drawing.Printing.PrintDocument();
+            this.ppdProduto = new System.Windows.Forms.PrintPreviewDialog();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.relatoriosToolStripMenuItem,
+            this.sair});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1235, 24);
+            this.menuStrip2.TabIndex = 0;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuario,
+            this.cliente,
+            this.produtos});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(71, 20);
+            this.toolStripMenuItem1.Text = "Cadastros";
+            // 
+            // usuario
+            // 
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(122, 22);
+            this.usuario.Text = "Usuários";
+            this.usuario.Click += new System.EventHandler(this.usuario_Click);
+            // 
+            // cliente
+            // 
+            this.cliente.Name = "cliente";
+            this.cliente.Size = new System.Drawing.Size(122, 22);
+            this.cliente.Text = "Clientes";
+            this.cliente.Click += new System.EventHandler(this.cliente_Click);
+            // 
+            // produtos
+            // 
+            this.produtos.Name = "produtos";
+            this.produtos.Size = new System.Drawing.Size(122, 22);
+            this.produtos.Text = "Produtos";
+            this.produtos.Click += new System.EventHandler(this.produtos_Click);
+            // 
+            // relatoriosToolStripMenuItem
+            // 
+            this.relatoriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuario2,
+            this.cliente2,
+            this.produtos2});
+            this.relatoriosToolStripMenuItem.Name = "relatoriosToolStripMenuItem";
+            this.relatoriosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.relatoriosToolStripMenuItem.Text = "Relatorios";
+            // 
+            // usuario2
+            // 
+            this.usuario2.Name = "usuario2";
+            this.usuario2.Size = new System.Drawing.Size(180, 22);
+            this.usuario2.Text = "Usuários";
+            this.usuario2.Click += new System.EventHandler(this.usuario2_Click);
+            // 
+            // cliente2
+            // 
+            this.cliente2.Name = "cliente2";
+            this.cliente2.Size = new System.Drawing.Size(180, 22);
+            this.cliente2.Text = "Clientes";
+            this.cliente2.Click += new System.EventHandler(this.cliente2_Click);
+            // 
+            // produtos2
+            // 
+            this.produtos2.Name = "produtos2";
+            this.produtos2.Size = new System.Drawing.Size(180, 22);
+            this.produtos2.Text = "Produtos";
+            // 
+            // sair
+            // 
+            this.sair.Name = "sair";
+            this.sair.Size = new System.Drawing.Size(38, 20);
+            this.sair.Text = "Sair";
+            this.sair.Click += new System.EventHandler(this.sair_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monospac821 BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(584, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Exército Brasileiro: Braço forte, mão amiga.";
+            // 
+            // pdUsuario
+            // 
+            this.pdUsuario.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pdUsuario_PrintPage);
+            // 
+            // ppdUsuario
+            // 
+            this.ppdUsuario.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.ppdUsuario.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.ppdUsuario.ClientSize = new System.Drawing.Size(400, 300);
+            this.ppdUsuario.Document = this.pdUsuario;
+            this.ppdUsuario.Enabled = true;
+            this.ppdUsuario.Icon = ((System.Drawing.Icon)(resources.GetObject("ppdUsuario.Icon")));
+            this.ppdUsuario.Name = "ppdUsuario";
+            this.ppdUsuario.Visible = false;
+            // 
+            // pdCliente
+            // 
+            this.pdCliente.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pdCliente_PrintPage);
+            // 
+            // ppdCliente
+            // 
+            this.ppdCliente.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.ppdCliente.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.ppdCliente.ClientSize = new System.Drawing.Size(400, 300);
+            this.ppdCliente.Document = this.pdCliente;
+            this.ppdCliente.Enabled = true;
+            this.ppdCliente.Icon = ((System.Drawing.Icon)(resources.GetObject("ppdCliente.Icon")));
+            this.ppdCliente.Name = "ppdCliente";
+            this.ppdCliente.Visible = false;
+            // 
+            // pdProduto
+            // 
+            this.pdProduto.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pdProduto_PrintPage);
+            // 
+            // ppdProduto
+            // 
+            this.ppdProduto.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.ppdProduto.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.ppdProduto.ClientSize = new System.Drawing.Size(400, 300);
+            this.ppdProduto.Enabled = true;
+            this.ppdProduto.Icon = ((System.Drawing.Icon)(resources.GetObject("ppdProduto.Icon")));
+            this.ppdProduto.Name = "ppdProduto";
+            this.ppdProduto.Visible = false;
+            // 
+            // Principal
+            // 
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1235, 681);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.menuStrip2);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip2;
+            this.Name = "Principal";
+            this.Text = "Cadastro Exército";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuáriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatóriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuáriosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem usuario;
+        private System.Windows.Forms.ToolStripMenuItem cliente;
+        private System.Windows.Forms.ToolStripMenuItem produtos;
+        private System.Windows.Forms.ToolStripMenuItem relatoriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuario2;
+        private System.Windows.Forms.ToolStripMenuItem cliente2;
+        private System.Windows.Forms.ToolStripMenuItem produtos2;
+        private System.Windows.Forms.ToolStripMenuItem sair;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Drawing.Printing.PrintDocument pdUsuario;
+        private System.Windows.Forms.PrintPreviewDialog ppdUsuario;
+        private System.Drawing.Printing.PrintDocument pdCliente;
+        private System.Windows.Forms.PrintPreviewDialog ppdCliente;
+        private System.Drawing.Printing.PrintDocument pdProduto;
+        private System.Windows.Forms.PrintPreviewDialog ppdProduto;
+    }
+}
+
---
+++ atividadeEduDS/AtCadastroAeS/AtCadastroAeS/AtCadastroAeS/Principal.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtCadastroAeS
+{
+    public partial class Principal : Form
+    {
+        public struct Usuar
+        {
+            public int codigo;
+            public string nome;
+            public string nivel;
+            public string login;
+            public string senha;
+        }
+
+        static public Usuar[] usuarios = new Usuar[10];
+        static public int contUsuario = 0;
+
+        public struct CadasC
+        {
+            public int codigo;
+            public string nome;
+            public string cpf;
+            public string rg;
+            public string end;
+            public string bairro;
+            public string cidade;
+            public string uf;
+            public string telefone;
+            public string email;
+            public string cep;
+        }
+
+        static public CadasC[] cadastro = new CadasC[10];
+        static public int contCadastro = 0;
+
+        public struct CadasP
+        {
+            public int codigo;
+            public string desc;
+            public string uni;
+            public string qtd;
+            public string pcC;
+            public string pcV;
+        }
+
+        static public CadasP[] cadastrop = new CadasP[10];
+        static public int contCadastrop = 0;
+
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void usuario_Click(object sender, EventArgs e)
+        {
+            Usuario fu = new Usuario();
+            fu.ShowDialog();
+        }
+
+        private void cliente_Click(object sender, EventArgs e)
+        {
+            CadastroC lol = new CadastroC();
+            lol.ShowDialog();
+        }
+
+        private void produtos_Click(object sender, EventArgs e)
+        {
+            CadastroP lmao = new CadastroP();
+            lmao.ShowDialog();
+        }
+
+
+
+        private void pdUsuario_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string strDados = "";
+            Graphics objImpressao = e.Graphics;
+            int pag = 0, linha = 0, i = 0;
+            bool cabecalho = true, itens = true;
+
+
+
+            while (cabecalho)
+            {
+                strDados = "                             RELATÓRIO DE USUÁRIOS" + (char)10;
+                strDados += "Data: " + DateTime.Now.ToString("dd/MM/yyyy") + "                                                         Pág: " + pag.ToString("00") + (char)10;
+                strDados += "--------------------------------------------------------------------------------" + (char)10;
+                strDados += "Código Nome                                     Nível Login" + (char)10;
+                strDados += "--------------------------------------------------------------------------------" + (char)10;
+                linha = 5;
+                itens = true;
+                while (itens)
+                {
+                    strDados += usuarios[i].codigo.ToString("000000") + " " + usuarios[i].nome.PadRight(40) + "   " + usuarios[i].nivel + "   " + usuarios[i].login + (char)10;
+                    linha++;
+                    i++;
+                    if (linha >= 64)
+                    {
+                        itens = false;
+                        strDados += (char)12;
+                    }
+                    if (i >= contUsuario)
+                    {
+                        itens = false;
+                        cabecalho = false;
+                    }
+                }
+            }
+            objImpressao.DrawString(strDados, new Font("Courier New", 11, FontStyle.Regular), Brushes.Black, 50, 50);
+        }
+
+        private void usuario2_Click(object sender, EventArgs e)
+        {
+            ppdUsuario.ShowDialog();
+        }
+
+        private void pdCliente_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string strDados = "";
+            Graphics objImpressao = e.Graphics;
+            int pag = 0, linha = 0, i = 0;
+            bool cabecalho = true, itens = true;
+            while (cabecalho)
+            {
+                strDados = "                             RELATÓRIO DE CLIENTES " + (char)10;
+                strDados += "Data: " + DateTime.Now.ToString("dd/MM/yyyy") + "                                                         Pág: " + pag.ToString("00") + (char)10;
+                strDados += "--------------------------------------------------------------------------------" + (char)10;
+                strDados += "Código Nome CPF RG Endereço Bairro Cidade UF CEP Telefone Email ";
+                strDados += "--------------------------------------------------------------------------------" + (char)10;
+                linha = 5;
+                itens = true;
+                while (itens)
+                {
+                    strDados += cadastro[i].codigo.ToString("000000") + " " + cadastro[i].nome.PadRight(40) + "   " + cadastro[i].cpf + "   " + cadastro[i].rg + "   " + cadastro[i].end + "   " + cadastro[i].bairro + "   " + cadastro[i].cidade + "   " + cadastro[i].uf + "   " + cadastro[i].cep + "   " + cadastro[i].telefone + "   " + cadastro[i].email + (char)10;
+                    linha++;
+                    i++;
+                    if (linha >= 64)
+                    {
+                        itens = false;
+                        strDados += (char)12;
+                    }
+                    if (i >= contCadastro)
+                    {
+                        itens = false;
+                        cabecalho = false;
+                    }
+                }
+
+
+            }
+            objImpressao.DrawString(strDados, new Font("Courier New", 11, FontStyle.Regular), Brushes.Black, 50, 50);
+        }
+
+        private void cliente2_Click(object sender, EventArgs e)
+        {
+            ppdCliente.ShowDialog();
+        }
+
+        private void pdProduto_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string strDados = "";
+            Graphics objImpressao = e.Graphics;
+            int pag = 0, linha = 0, i = 0;
+            bool cabecalho = true, itens = true;
+            while (cabecalho)
+            {
+                strDados = "                             RELATÓRIO DE PRODUTOS " + (char)10;
+                strDados += "Data: " + DateTime.Now.ToString("dd/MM/yyyy") + "                                                         Pág: " + pag.ToString("00") + (char)10;
+                strDados += "--------------------------------------------------------------------------------" + (char)10;
+                strDados += "Código   Descrição   Unidade   Quantidade   Preço de Custo   Preço de Venda ";
+                strDados += "--------------------------------------------------------------------------------" + (char)10;
+                linha = 5;
+                itens = true;
+            }
+            while (itens)
+            {
+                strDados += cadastro[i].codigo.ToString("000000") + " " + cadastrop[i].desc.PadRight(40) + "   " + cadastrop[i].uni + "   " + cadastrop[i].qtd + "   " + cadastrop[i].pcC + "   " + cadastrop[i].pcV + (char)10;
+                linha++;
+                i++;
+                if (linha >= 64)
+                {
+                    itens = false;
+                    strDados += (char)12;
+                }
+                if (i >= contCadastrop)
+                {
+                    itens = false;
+                    cabecalho = false;
+                }
+            }
+            objImpressao.DrawString(strDados, new Font("Courier New", 11, FontStyle.Regular), Brushes.Black, 50, 50);
+        }
+    }
+}
+
+    
+
+
